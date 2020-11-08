4137e96783ebbad460edf17b5c1c010bed5ac452
Elisenda <elisenda.monserrat@gmail.com>
2020-11-08 09:33:34 +0100

    minimized the background image

--- javascript/background.js
+++ javascript/background.js
@@ -7,15 +7,12 @@ class Background {
 
     draw() {
         this.backgroundImg.src = "./images/gameboard2-minmin.png";
-        this.backgroundImg.addEventListener("load", () => {
-            //console.log("se carga la imagen");
-            this.ctx.drawImage(
-                this.backgroundImg,
-                this.x,
-                this.y,
-                600,
-                600
-            );
-        }); 
+        this.ctx.drawImage(
+            this.backgroundImg,
+            this.x,
+            this.y,
+            600,
+            600
+        );
     }
 }
